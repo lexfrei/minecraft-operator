62135fd0235e146590809b6876f02e09a74b5470
Aleksei Sviridkin <f@lex.la>
2026-03-13 22:46:04 +0300

    fix(controller): register apiextensions/v1 in scheme for CRD manager (#66)
    ## Summary
    
    - CRD manager uses typed `apiextensionsv1.CustomResourceDefinition` in
    `WaitEstablished` but the type was not registered in the runtime scheme
    - This caused operator crash at startup with `no kind is registered for
    the type v1.CustomResourceDefinition in scheme` error
    - Add `apiextensionsv1.AddToScheme(scheme)` to the init function
    
    ## Test plan
    
    - [ ] Verify CI passes
    - [ ] Deploy v1.0.2 to cluster, confirm operator starts successfully
    
    Signed-off-by: Aleksei Sviridkin <f@lex.la>

--- cmd/main.go
+++ cmd/main.go
@@ -27,6 +27,7 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
 	"github.com/go-logr/logr"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
@@ -67,6 +68,7 @@ var (
 
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	utilruntime.Must(apiextensionsv1.AddToScheme(scheme))
 
 	utilruntime.Must(mck8slexlav1beta1.AddToScheme(scheme))
 	utilruntime.Must(volumesnapshotv1.AddToScheme(scheme))
